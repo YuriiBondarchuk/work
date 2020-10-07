307cda56f194e140294c668c2b416c562102e19e
YBond <ymbondarchuk@gmail.com>
2020-10-08 00:41:05 +0300

    Added bootstrap module

--- yii/views/layouts/main.php
+++ yii/views/layouts/main.php
@@ -1,13 +1,34 @@
+<?php $this->beginPage() ?>
 <!doctype html>
 <html lang="en">
 <head>
+    <?php $this->head() ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 <body>
-<?= $content ?>
+<?php $this->beginBody() ?>
+<?
+\yii\bootstrap\NavBar::begin([
+    'brandLabel' => 'ITVDN',
+    'brandUrl'   => Yii::$app->homeUrl,
+    'options'    => [
+        'class' => 'navbar-default navbar-fixed-top',
+    ],
+
+]);
+\yii\bootstrap\NavBar::end();
+?>
+
+<div class="container" style="margin-top: 60px">
+    <?= $content ?>
+</div>
+
+<?php $this->endBody() ?>
 </body>
 </html>
+
+<?php $this->endPage() ?>
 
